d6185f2fd069e6be3028ba89df88471972c33acb
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-09-21 14:49:28 +0000

    Added signup at auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,4 +2,20 @@ const express = require('express');
 const passport = require('passport');
 const router = express.Router();
 
+// Sign Up
+router.get('/signup', function(req,res) {
+  res.render('signup', {
+    title: "Signup",
+    message: "Signed Up"
+  });
+});
+
+router.post('/signup', passport.authenticate('local-signup'), {
+  successRedirect: 'auth/login',
+  failureRedirect: 'auth/signup',
+  failfureFlash: true
+});
+
+
+
 module.exports = router;

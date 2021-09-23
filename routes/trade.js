cd64fff4711495ce1c95f339d6ebb834e80e938f
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-09-23 14:37:23 +0000

    Added accept trade request at trade.js

--- routes/trade.js
+++ routes/trade.js
@@ -11,6 +11,29 @@ router.get('/',isLoggedIn, function(req,res) {
   });
 });
 
+// Accept Trade Request
+router.post('/acceptRequest/:tradeID',isLoggedIn, function(req,res) {
+  let trade_id = req.params.tradeID;
+  // Find the Trade
+  Trade.findById(trade_id).populate({path:'book',model:'book'}).exec(function(err,trade) {
+    let book_id = trade.book._id;
+    // Search Book ID
+    Book.findById(book_id).populate({path:'owner',model:'user'}).exec(function(err,book) {
+      // Update new owner for book owner & book status
+      let theOldOwnerId = book.owner;
+      let theNewOwnerId = trade.from;
+      book.owner = theNewOwnerId;
+      book.status = 'available';
+      book.save(function(err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+      });
+    });
+  });
+});
+
 // LogIn function
 function isLoggedIn(req,res,next) {
   if (req.isAuthenticated()) {

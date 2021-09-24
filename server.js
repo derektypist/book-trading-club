957536f468edaf5683170cd68da2cb88a01375cb
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-09-24 15:45:30 +0000

    Added app.use at server.js

--- server.js
+++ server.js
@@ -5,3 +5,8 @@ const app = express();
 
 // Set Up Template Engine
 app.set('view engine','pug');
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(passport.initialize());
+app.use(passport.session());

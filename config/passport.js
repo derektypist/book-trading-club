61274795e59e45b75e0ea7b339eb8e327de65732
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-09-24 16:55:24 +0000

    Added serialization and deserialization at passport.js

---
+++ config/passport.js
@@ -0,0 +1,17 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const User = require('../models').User;
+
+module.exports = function(passport) {
+
+  // Serialization and Deserialization
+  passport.serializeUser(function(user,done) {
+    done(null,user.id);
+  });
+
+  passport.deserializeUser(function(id,done) {
+    User.findById(id, function(err,user) {
+      done(err,user);
+    });
+  });
+};

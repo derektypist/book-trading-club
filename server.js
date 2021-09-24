141f6447fd1db273954a219fb7f6baa776b734e6
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-09-24 15:56:51 +0000

    Updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const books = require('google-books-search');
 const app = express();
 
 // Set Up Template Engine
@@ -10,3 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(passport.initialize());
 app.use(passport.session());
+app.use(flash());
+app.use(function(req,res,next) {
+  res.locals.user = req.user();
+  next();
+});

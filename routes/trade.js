2d73013cd7063c517b32605c84efee1dde6dad88
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-09-24 11:35:45 +0000

    Added route to rejected request at trade.js

--- routes/trade.js
+++ routes/trade.js
@@ -69,6 +69,35 @@ router.post('/acceptRequest/:tradeID',isLoggedIn, function(req,res) {
   });
 });
 
+// Reject the Request
+router.post('/rejectRequest/:tradeID', isLoggedIn, function(req,res) {
+  let trade_id = req.params.tradeID;
+  Trade.findById(trade_id).populate({path:'book',model:'book'}).exec(function(err,trade) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    let book_id = trade.book._id;
+    Book.findById(book_id).exec(function(err,book) {
+      book.status = 'available';
+      book.save(function(err) {
+        if (err) {
+          console.log(err);
+          return;
+        }
+        trade.remove(function(err) {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          res.redirect('/allbooks');
+        });
+      });
+    });
+  });
+  res.send('rejected request');
+});
+
 // LogIn function
 function isLoggedIn(req,res,next) {
   if (req.isAuthenticated()) {

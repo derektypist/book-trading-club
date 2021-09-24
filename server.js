2ba03d16f71f14adc7b15bfff3b807955605bf43
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-09-24 15:35:14 +0000

    Added template engine at server.js

--- server.js
+++ server.js
@@ -2,3 +2,6 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const app = express();
+
+// Set Up Template Engine
+app.set('view engine','pug');

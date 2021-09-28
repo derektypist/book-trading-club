41edf75210f25b3d173a5cc59a86aa2dcf81169f
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-09-28 10:47:16 +0000

    Updated sign up strategy at passport.js

--- config/passport.js
+++ config/passport.js
@@ -20,5 +20,27 @@ module.exports = function(passport) {
     usernameField: 'email',
     passwordField: 'password',
     passReqToCallback: true
+  }, function(req,email,password,done) {
+    process.nextTick(function() {
+      User.findOne({'local.email':email}, function(err,user) {
+        if (err) {
+          return done(err);
+        }
+        if (user) {
+          return done(null,false, req.flash('signupMessage','This email has already been taken'));
+        } else {
+          let theNewUser = new User();
+          theNewUser.local.username = req.body.username;
+          theNewUser.local.email = email;
+          theNewUser.local.password = theNewUser.genHash(password);
+          theNewUser.save(function(err) {
+            if (err) {
+              throw (err);
+            }
+            return done(null, theNewUser);
+          });
+        }
+      });
+    });
   }));
 };

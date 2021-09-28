ee7f67e41edeeea87ebfb1d6d8b68ce03e1f3dc9
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-09-28 11:11:58 +0000

    Updated server.js

--- server.js
+++ server.js
@@ -8,9 +8,11 @@ const app = express();
 
 // Set Up Template Engine
 app.set('view engine','pug');
+app.set('views','./views');
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());

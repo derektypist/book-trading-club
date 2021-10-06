57bedcff7820eedc28641f4fdba2d5e6a6d28e54
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-10-06 14:43:34 +0000

    Made minor changes at server.js

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
 app.use(function(req,res,next) {
-  res.locals.user = req.user();
+  res.locals.user = req.user;
   next();
 });
 
